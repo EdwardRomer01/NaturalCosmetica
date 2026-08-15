54acd523c7cc09eaecbf85488b293b8c7b4ee1a0
edwar <edward.romero686@std.uni.edu.ni>
2026-08-14 19:42:54 -0600

    Segundo commit edu

--- NaturalCosmeticaUNI/Form1.Designer.cs
+++ NaturalCosmeticaUNI/Form1.Designer.cs
@@ -29,6 +29,9 @@
         private void InitializeComponent()
         {
             label1 = new Label();
+            listBox1 = new ListBox();
+            pictureBox1 = new PictureBox();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
             SuspendLayout();
             // 
             // label1
@@ -40,13 +43,34 @@
             label1.TabIndex = 0;
             label1.Text = "Natural Cosmetic";
             // 
+            // listBox1
+            // 
+            listBox1.FormattingEnabled = true;
+            listBox1.Location = new Point(148, 177);
+            listBox1.Name = "listBox1";
+            listBox1.Size = new Size(150, 104);
+            listBox1.TabIndex = 1;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = Properties.Resources.Acer_Wallpaper_01_3840x2400___Copy;
+            pictureBox1.Location = new Point(406, 187);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(236, 142);
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.TabIndex = 2;
+            pictureBox1.TabStop = false;
+            // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(pictureBox1);
+            Controls.Add(listBox1);
             Controls.Add(label1);
             Name = "Form1";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
             ResumeLayout(false);
             PerformLayout();
         }
@@ -54,5 +78,7 @@
         #endregion
 
         private Label label1;
+        private ListBox listBox1;
+        private PictureBox pictureBox1;
     }
 }
